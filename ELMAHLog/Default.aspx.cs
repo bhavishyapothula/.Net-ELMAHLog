49a8000f2f86494f727b12f162024ab24d97769b
Bhavishya Pothula <Bhavishya_Pothula@epam.com>
2020-02-04 15:15:17 +0530

    Add project files.

---
+++ ELMAHLog/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Elmah;
+
+namespace ELMAHLog
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //Throwing divide by zero exception
+                throw new DivideByZeroException();
+            }
+            catch(Exception ex)
+            {
+                //Handling exception
+                ErrorSignal.FromCurrentContext().Raise(ex);
+            }
+
+        }
+    }
+}
---
+++ ELMAHLog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ELMAHLog.Global" Language="C#" %>
